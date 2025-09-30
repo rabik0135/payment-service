2607a4ed35ec654959d318693be1393354d6eadc
Ivan Rabinchuk <ivan.rabinchuk@gmail.com>
2025-09-30 22:04:45 +0300

    db_layer

---
+++ src/main/java/com/rabinchuk/paymentservice/model/Payment.java
@@ -0,0 +1,46 @@
+package com.rabinchuk.paymentservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Document(collection = "payments")
+public class Payment {
+    @Id
+    private String id;
+
+    @Indexed
+    @Field("order_id")
+    private Long orderId;
+
+    @Indexed
+    @Field("user_id")
+    private Long userId;
+
+    @Indexed
+    private PaymentStatus status;
+
+    private LocalDateTime timestamp;
+
+    @Field("payment_amount")
+    private BigDecimal paymentAmount;
+
+}
---
+++ src/main/java/com/rabinchuk/paymentservice/model/PaymentStatus.java
@@ -0,0 +1,6 @@
+package com.rabinchuk.paymentservice.model;
+
+public enum PaymentStatus {
+    SUCCESS,
+    FAILED
+}
